549868a6826ed69416b9d1c8dc2c252176e95f6d
Dilan Kubay <dilankubay1@gmail.com>
2024-12-04 18:44:26 +0300

    changes

--- thresholding.py
+++ thresholding.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-# Görüntüyü okuma (gri tonlamalı olarak)
+# Görüntüyü okuma (renkli olarak)
 input_image_path = "Images/Lenna.png"
-image = cv2.imread(input_image_path, cv2.IMREAD_GRAYSCALE)
+image = cv2.imread(input_image_path, cv2.IMREAD_COLOR)
+
+# Görüntüyü gri tonlamalı hale getirme
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Görüntü boyutlarını al
-height, width = image.shape
+height, width = gray_image.shape
 
 # Eşik değeri belirleme
 esikDeger = 100
@@ -17,7 +20,7 @@ output_image = np.zeros((height, width), dtype=np.uint8)
 # Eşikleme işlemi
 for y in range(height):
     for x in range(width):
-        if image[y, x] > esikDeger:
+        if gray_image[y, x] > esikDeger:
             output_image[y, x] = 255  # Beyaz
         else:
             output_image[y, x] = 0    # Siyah
