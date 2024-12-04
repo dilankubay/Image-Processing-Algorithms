9e6f4ee4bad53f3d1c6397daa1c1b987d4f5d8ff
Dilan Kubay <dilankubay1@gmail.com>
2024-12-04 16:50:06 +0300

    thresholding

---
+++ thresholding.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+# Görüntüyü okuma (gri tonlamalı olarak)
+input_image_path = "photo_gray.png"
+image = cv2.imread(input_image_path, cv2.IMREAD_GRAYSCALE)
+
+# Görüntü boyutlarını al
+height, width = image.shape
+
+# Eşik değeri belirleme
+esikDeger = 100
+
+# Yeni bir çıktı görüntüsü oluştur
+output_image = np.zeros((height, width), dtype=np.uint8)
+
+# Eşikleme işlemi
+for y in range(height):
+    for x in range(width):
+        if image[y, x] > esikDeger:
+            output_image[y, x] = 255  # Beyaz
+        else:
+            output_image[y, x] = 0    # Siyah
+
+# Orijinal ve işlenmiş görüntüyü gösterme
+cv2.imshow("Orijinal Görüntü", image)
+cv2.imshow("İşlenmiş Görüntü", output_image)
+
+# Çıkış için bir tuşa basmayı bekleme
+cv2.waitKey(0)
+cv2.destroyAllWindows()
